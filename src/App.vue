a6c9fc02a4b74523518eadc1e8f9aecdb10c1af4
AidenWik <142683824+AidenWik@users.noreply.github.com>
2023-09-19 13:11:04 -0400

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,79 @@
 <template>
-  <!----<img alt="Vue logo" src="./assets/logo.png">-->
-  <HelloWorld msg="HELLO WORLD"/>
-
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  <head>
+   <title>Homework IST 256 Sheetz</title>
+ </head>
+ <body>
+  <div class="card-container">
+   <div class="card-wrapper">
+   <div class="Sheetz">
+     <label class="sheetzLbl">Sheetz</label>
+   </div>
+   
+ <div class="directions">
+   <label id="directionsLbl">Directions</label>
+ </div>
+   
+ <div class="Favorites">
+   <label id="Favoriteslbl">Favorites</label>
+   </div>
+   
+ <div class="btn-wrapper">
+   <button>Hotdog</button>
+   <button>Fries</button>
+   <button>Shake</button>
+ </div>
+ 
+ <div class="images">
+   <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Hot_dog_with_mustard.png" alt="A beautiful landscape">
+ 
+   <img src="https://media.istockphoto.com/id/496094410/photo/potatoes-fries-in-a-red-carton-box-isolated-fast-food.jpg?s=612x612&w=0&k=20&c=d7WnO6AW0C3A-xMSBa3Yb5rmSNelyY-jmxhW2efZq2Q=">
+ 
+   <img 
+ src="https://gofericecream.com/wp-content/uploads/2021/06/shake-.png"> 
+   
+ </div>
+ 
+  <div class="detailsBtn">
+         <button class="toggle-details">Details</button>
+     </div>
+      
+   
+ <div class="paragraphs">
+   <p1>$1.99 <br>A food consisting of a grilled or steamed sausage served in the slit of a partially sliced bun.</p1>
+   <p2>$0.99 <br>Side dish or snack typically made from deep-fried potatoes that have been cut into various shapes, especially thin strips.</p2>
+   <p3>$5.99 <br> A sweet beverage made by blending milk, ice cream, and flavorings or sweeteners.</p3>
+   </div>
+ 
+  
+   </div>
+ 
+   </div>
+   
+   <div class ="duper">
+    <button id="duper">Button for duplication</button>
+     </div>   
+   
+   
+    
+   
+   <div class ="background">
+     <button id="background">Change Background</button>
+   </div>
+    
+   
+   <div class ="title">
+     <button id="title">Change Title</button>
+   </div>
+   
+ 
+   
+     <div class="delete">
+         <button id="deleteLastCard">Delete Last Card</button>
+     </div>
+     
+    
+ 
+ 
+   </body>
+ </template>
+ 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,74 @@
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+
+// JavaScript to toggle visibility of the paragraph descriptions
+const toggleDetailsButtons = document.querySelectorAll('.toggle-details');
+const paragraphsSections = document.querySelectorAll('.paragraphs');
+
+toggleDetailsButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    paragraphsSections[index].classList.toggle('hidden');
+  });
+});
+
+// JavaScript for cloning cards
+document.querySelector('#duper').addEventListener('click', (e) => {
+  const cardContainer = document.querySelector('.card-container');
+  const cardWrapperClone = document.querySelector('.card-wrapper').cloneNode(true);
+
+  // Add unique classes to the cloned elements
+  cardWrapperClone.classList.add('cloned-card');
+  cardWrapperClone.querySelector('.toggle-details').textContent = 'Details';
+
+  cardContainer.appendChild(cardWrapperClone);
+
+  // Set up event listeners for the cloned button
+  const clonedToggleDetailsButton = cardWrapperClone.querySelector('.toggle-details');
+  const clonedParagraphsSection = cardWrapperClone.querySelector('.paragraphs');
+
+  clonedToggleDetailsButton.addEventListener('click', () => {
+    clonedParagraphsSection.classList.toggle('hidden');
+  });
+});
+document.querySelector('#deleteLastCard').addEventListener('click', (e) => {
+// Code for deleting the last card
+const cardContainer = document.querySelector('.card-container');
+const cardWrappers = document.querySelectorAll('.card-wrapper');
+
+// Check if there's at least one card to delete
+if (cardWrappers.length > 1) {
+    const lastCard = cardWrappers[cardWrappers.length - 1];
+    cardContainer.removeChild(lastCard);
+}
+});
+
+document.querySelector('.card-wrapper').addEventListener('mouseover', (e) => {
+const cardWrapper = document.querySelector('.card-wrapper');
+cardWrapper.classList.toggle('alt-bg2');
+});
+
+document.querySelector('.card-wrapper').addEventListener('mouseleave', (e) => {
+const cardWrapper = document.querySelector('.card-wrapper');
+cardWrapper.classList.toggle('alt-bg2');
+});
+
+document.querySelector('#title').addEventListener('click', (e) => {
+const headings = document.querySelectorAll('.sheetzLbl'); // Corrected class name
+headings.forEach((heading) => {
+heading.textContent = "something else";
+});
+});
+
+
+
+document.querySelector('#background').addEventListener('click', (e) => {
+const cardWrappers = document.querySelectorAll('.card-wrapper');
+/* loop to change each existing iteration*/
+cardWrappers.forEach((cardWrapper) => {
+  cardWrapper.classList.toggle('alt-bg');
+});
+});
